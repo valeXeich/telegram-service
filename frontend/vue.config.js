c00e44e34254b84fae2bf874bed750bb526f7842
valex <valerapanow03@gmail.com>
2023-02-04 20:39:31 +0200

    vue config 0.5

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+}
 
 
