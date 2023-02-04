c14779c2b0ca5dd44f53c53f8e85a29cabd87b7f
valex <valerapanow03@gmail.com>
2023-02-04 20:50:00 +0200

    vue config 0.6

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true
-  }
-}
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    disableHostCheck: true,
+  },
+  transpileDependencies: true,
+};
 
 
